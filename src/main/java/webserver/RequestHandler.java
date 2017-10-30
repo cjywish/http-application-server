37c52db1b9aae447fc52ef450e902205175ed2ff
cjywish <30422987+cjywish@users.noreply.github.com>
2017-10-30 10:10:31 +0900

    add comments for git pull errors test

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -24,6 +24,7 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	// comments for git pull error test
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Welcome to JavaW World ==> Http application server by cjywish".getBytes();
             response200Header(dos, body.length);
